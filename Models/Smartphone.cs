e36fb3d5f4c620077f3896bae16a456ef5091f7f
Ale015 <veadrigojunior@gmail.com>
2024-10-11 18:41:48 -0300

    Resolução das requisicoes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,25 +2,33 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
-        {
+        //Construtor
+        public Smartphone(string numero, string modelo, string imei, int memoria){
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
+        // Atributos
+        public string Numero {get; set;}
+        public string Modelo {get; set;}
+        protected string IMEI {get; set;}
+        protected int Memoria {get; set;}
+
+
+        // Métodos
+        public void Ligar(){
+            System.Console.WriteLine($"Ligando...");
         }
 
-        public void ReceberLigacao()
-        {
+        public void ReceberLigacao(){
             Console.WriteLine("Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
     }
 }
+      
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia tijolao = new Nokia("(99)99999-8888","Tijolo 3210","111111111",128);
+
+tijolao.Numero = "(11)11111-1111";
+System.Console.WriteLine(tijolao.Numero);
+System.Console.WriteLine(tijolao.Modelo);
+
+
+tijolao.InstalarAplicativo("Whatsapp");
+tijolao.Ligar();
+tijolao.ReceberLigacao();
+
+// tijolao.IMEI = "A tentativa de nova atribuição gera erro";
+// tijolao.Memoria = "A tentativa de nova atribuição gera erro";
+
+
+System.Console.WriteLine("");
+
+Iphone iphone = new Iphone("(88)88888-8888","15 Pro Max","222222222",256);
+
+System.Console.WriteLine(iphone.Numero);
+System.Console.WriteLine(iphone.Modelo);
+
+
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+// iphone.IMEI = "A tentativa de nova atribuição gera erro";
+// iphone.Memoria = "A tentativa de nova atribuição gera erro";
